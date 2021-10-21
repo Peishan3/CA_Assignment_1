5534f8b6cc29784da5dca3f46586b0951ac78e97
Peishan3 <92852023+Peishan3@users.noreply.github.com>
2021-10-21 16:58:22 +0800

    Update Length_of_Last_Word.c

--- Length_of_Last_Word.c
+++ Length_of_Last_Word.c
@@ -2,22 +2,22 @@
 
 int lengthOfLastWord(char * s)
 {
-    while(*s != 0)
+    while(*s != 0) //finding the end of string
     {
         s++;
     }
     int position = 0;
-    s--;
-    while(*s == 32)
+    s--; //Because last word in the string will be '/0', go back for one char
+    while(*s == 32)// if there are some space in the end, keep going back for word
     {
         s--;
     }
-    while(*s != 32)
+    while(*s != 32) //After finding the word, we can count for how long is the word here
     {
         position++;
         s--;
     }
-    printf("Length of the last word is %d", position);
+    printf("Length of the last word is %d", position);//print result
 
 }
 
